b0f7765a446e09be197f00d791baceae6b5902d1
Dmitriy <yadrin.d@gmail.com>
2023-10-17 23:49:43 +0600

    Public facing pages localization

---
+++ lang/en/forms.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+
+    'name_t' => 'Name',
+    'email_t' => 'Email',
+    'password_t' => 'Password',
+    'confirm_pass_t' => 'Confirm Password',
+    'already_reg_t' => 'Already registered?'
+];
--- lang/ru/auth.php
+++ lang/ru/auth.php
@@ -13,9 +13,9 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Неверное имя пользователя или пароль..',
+    'password' => 'Некорректный пароль.',
+    'throttle' => 'Слишком много попыток входа. Пожалуйста, попробуйте ещё раз через :seconds секунд.',
     'email_t' => 'Электронная почта',
     'password_t' => 'Пароль',
     'remember_t' => 'Запомнить пароль',
